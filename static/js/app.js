e4e664edc5174cda53d5cfd3ca3eaabc16c7cbfe
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2024-10-24 15:27:16 +0100

    project complete with SEO

---
+++ static/js/app.js
@@ -0,0 +1,12 @@
+const mode = localStorage.getItem("mode") || "";
+
+const toggle = document.querySelector(".toggle");
+const body = document.querySelector("body");
+
+document.body.className = mode;
+
+toggle.addEventListener("click", () => {
+  console.log("click");
+  localStorage.setItem("mode", mode === "light" ? "" : "light");
+  body.classList.toggle("light");
+});
